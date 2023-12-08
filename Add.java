344b2d61dd8dd7eef9d7906326a690b616e7f63e
Saikumar Reddy <saikumar_reddy@thbs.india.com>
2023-12-08 14:49:01 +0530

    Java Add

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        int number1=10;
+        int  number2=22;
+        int sum=number1+number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
