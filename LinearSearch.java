77f57ec1bb91436859b3abc870e73926ef6dd9d5
miduturusaikumar <153052052+miduturusaikumar@users.noreply.github.com>
2023-12-08 15:12:10 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -10,10 +10,8 @@ class LinearSearch
         int arr[]={1,2,3,4,5,6};
         
         int tar=6;
-        for(int i=0;i<arr.length;i++)
-        {
-            arr[i]=sc.nextInt();
-        }
+
+        
         for(int i=0;i<arr.length;i++)
         {
              System.out.print(arr[i]+" ");
